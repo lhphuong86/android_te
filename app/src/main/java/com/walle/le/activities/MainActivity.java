a1649082a69eb75c46a84004565f8c950c0ae259
walle <lhphuong86@gmail.com>
2016-01-12 16:56:44 +0700

    fds

--- app/src/main/java/com/walle/le/activities/MainActivity.java
+++ app/src/main/java/com/walle/le/activities/MainActivity.java
@@ -1,11 +1,12 @@
-package le.walle.com.te;
+package com.walle.le.activities;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/walle/le/activities/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.walle.le.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.walle.le.activities.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2*1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+
+}
